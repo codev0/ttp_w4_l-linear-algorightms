d9387d02aa70fc9782161290ff56ad7b6b5157f3
Airat Zhanshuakov <dakanca@gmail.com>
2019-10-06 23:54:44 +0600

    first commit

---
+++ LinearAlgorithms.Tests/Test.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using System;
+namespace LinearAlgorithms.Tests
+{
+  [TestFixture()]
+  public class Test
+  {
+    [Test()]
+    public void TestCase()
+    {
+    }
+  }
+}
---
+++ LinearAlgorithms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinearAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var result = Calculate(3.4, 0.74, 19.43);
+          Console.WriteLine(result); 
+        }
+
+        private static double Calculate (double x, double y, double z)
+        {
+          double a = Math.Tan(x + y) * Math.Tan(x + y);
+          double b = Math.Exp(y - z);
+          double c = Math.Sqrt(Math.Cos(x * x) + Math.Sin(z * z));
+          double result = a - b * c;
+         
+          return result;
+        }
+    }
+}
